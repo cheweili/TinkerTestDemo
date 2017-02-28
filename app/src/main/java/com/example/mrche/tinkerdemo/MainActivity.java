e8f152f8dcea7328a2dd365621f6849dc67ec431
cheweili <18310278687@163.com>
2017-02-28 23:29:46 +0800

    完善 测试

--- app/src/main/java/com/example/mrche/tinkerdemo/MainActivity.java
+++ app/src/main/java/com/example/mrche/tinkerdemo/MainActivity.java
@@ -1,7 +1,15 @@
 package com.example.mrche.tinkerdemo;
 
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.tencent.tinker.lib.tinker.TinkerInstaller;
+
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +17,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView tv = (TextView) findViewById(R.id.tv);
+        tv.setText("修复好了");
+        Button fix = (Button) findViewById(R.id.fix);
+        fix.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TinkerInstaller.onReceiveUpgradePatch(getApplicationContext(), Environment.getExternalStorageDirectory().getAbsolutePath()
+                        + "/patch_signed_7zip.apk");
+                File file = new File(Environment.getExternalStorageDirectory().getAbsolutePath()
+                        + "/patch_signed_7zip.apk");
+                boolean exists = file.exists();
+                System.out.println(exists);
+            }
+        });
     }
 }
